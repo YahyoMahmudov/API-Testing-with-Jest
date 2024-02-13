5dce6f11abb0d78e5bf59c3c31dc83f6e44450c3
Yahyo Mahmudov <yahyomahmudovv@gmail.com>
2024-02-13 16:07:57 +0500

    Fixed unresolved issues

--- account/getAccountDetails.test.ts
+++ account/getAccountDetails.test.ts
@@ -1,13 +1,13 @@
 import { getAccountDetails } from './getAccountDetails';
 
-describe('fetchData function', () => {
-    
-    test('fetches data successfully', async () => {
+describe('Get account details', () => {
+
+    test('Verify that a account id is not null.', async () => {
         const data = await getAccountDetails();
-        expect(data.data.id).toBeDefined();
+        expect(data.data.id).not.toBe(null);
     });
 
-    test('Status code', async () => {
+    test('Verify that the status code of the response is 200', async () => {
         const { status } = await getAccountDetails();
         expect(status).toBe(200);
     });
--- account/getAccountDetails.ts
+++ account/getAccountDetails.ts
@@ -1,26 +1,41 @@
 import axios, { AxiosResponse } from 'axios';
 import { AccountDetailsI } from './accountDetailsI'
-import * as config from '../config.json';
+import dotenv from 'dotenv';
+dotenv.config();
 
-const baseUrl = config.api.baseUrl;
-const authToken = config.api.authToken;
-const accountId = config.api.accountId;
+const baseUrl = process.env.BASE_URL;
+const authToken = process.env.AUTH_TOKEN;
+const accountId = process.env.ACCOUNT_ID;
 
-export async function getAccountDetails(): Promise<AxiosResponse<AccountDetailsI>> {
-    const url = `${baseUrl}account/${accountId}`;
+export async function httpRequest<T>(
+    method: 'GET' | 'POST' | 'PUT' | 'DELETE',
+    url: string
+): Promise<AxiosResponse<T>> {
     const options = {
-        method: 'GET',
+        method,
         headers: {
             accept: 'application/json',
-            Authorization: `Bearer ${authToken}`
-        }
+            Authorization: `Bearer ${authToken}`,
+        },
     };
 
     try {
-        const response: AxiosResponse<AccountDetailsI> = await axios.get(url, options);
+        const response = await axios(url, options);
         return response;
     } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error(`Error making ${method} request to ${url}:`, error);
+        throw error;
+    }
+}
+
+export async function getAccountDetails(): Promise<AxiosResponse<AccountDetailsI>> {
+    const url = `${baseUrl}account/${accountId}`;
+
+    try {
+        const response = await httpRequest<AccountDetailsI>('GET', url);
+        return response;
+    } catch (error) {
+        console.error('Error fetching account details:', error);
         throw error;
     }
 }
