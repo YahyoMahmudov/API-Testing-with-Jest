beddc74117567047b70f23663075c2f22497a4d2
Ilhom <Ilhom.Islomov@inoviagroup.se>
2024-02-18 12:56:49 +0500

    clear console log

--- src/lists/lists.ts
+++ src/lists/lists.ts
@@ -2,36 +2,37 @@ import dotenv from "dotenv";
 import { AxiosResponse } from "axios";
 import { httpRequest } from "../httpMethod/httpRequest";
 import * as ResponseType from "./listDetails";
-import { MovieListDetails ,Status} from "./listDetails";
+import { MovieListDetails, Status } from "./listDetails";
 
 dotenv.config();
 
 const sessionId = process.env.SESSION_ID;
 
-export async function getMovieListDetails(listId:number, page:number=1): Promise<{data:MovieListDetails, status:number}> {
-    const url = `list/${listId}`;
-    const queryParam: any = {
-      language: "en-US",
-      page,
-    };
-  
-    try {
-      const response = await httpRequest<MovieListDetails>(
-        "GET",
-        url,
-        queryParam
-      );
-      return response;
-    } catch (error) {
-      console.error("Error fetching account details:", error);
-      throw error;
-    }
+export async function getMovieListDetails(
+  listId: number,
+  page: number = 1
+): Promise<{ data: MovieListDetails; status: number }> {
+  const url = `list/${listId}`;
+  const queryParam: any = {
+    language: "en-US",
+    page,
+  };
+
+  try {
+    const response = await httpRequest<MovieListDetails>(
+      "GET",
+      url,
+      queryParam
+    );
+    return response;
+  } catch (error) {
+    throw error;
   }
+}
 
-
-
-
-export async function clearItemsFromMovieList(listId:number): Promise<{data:Status, status:number }> {
+export async function clearItemsFromMovieList(
+  listId: number
+): Promise<{ data: Status; status: number }> {
   const url = `list/${listId}/clear`;
   const queryParam: any = {
     session_id: sessionId,
@@ -40,9 +41,8 @@ export async function clearItemsFromMovieList(listId:number): Promise<{data:Stat
 
   try {
     const response = await httpRequest<Status>("POST", url, queryParam);
-    return response
+    return response;
   } catch (error) {
     throw error;
   }
 }
-
